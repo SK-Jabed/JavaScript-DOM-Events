3135209a7d9db93adcb0157c8bd38001c5cca19f
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-15 23:52:05 +0600

    Event Summary Files Done

--- js/event-summary.js
+++ js/event-summary.js
@@ -2,9 +2,19 @@
 function handleOnClick() {
     const handlerStatus = document.getElementById("handler-status");
     handlerStatus.innerText = "Handled by Function Attached on onclick Function";
-}
+};
+
+// Option - 02 
 document.getElementById("event-listener").addEventListener("click", function() {
     const handlerStatus = document.getElementById("handler-status");
     handlerStatus.innerText = "Text Updated by Event Listener";
-})
+});
+//------------------------ OR -------------------------
+document.getElementById("update-btn").addEventListener("click", function() {
+    const inputField = document.getElementById("input-field");
+    const inputText = inputField.value;
 
+    const h4 = document.getElementById("update-input-text");
+    h4.innerText = inputText;
+    inputField.value = "";
+});
